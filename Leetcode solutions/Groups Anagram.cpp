78a576e5edcf57288caddace8f190b567e9b8794
Vishal Yadav <52189323+Vishalyadav0804@users.noreply.github.com>
2020-05-27 07:59:32 +0530

    Create Groups Anagram.cpp

---
+++ Leetcode solutions/Groups Anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+		map<string,vector<string>> m;
+		for(auto& x:strs){
+			auto tem=x;
+			sort(tem.begin(),tem.end());
+			m[tem].push_back(x);
+		}
+		for(auto v:m){
+			res.push_back(v.second);
+		}
+		return res;
+    }
+};
