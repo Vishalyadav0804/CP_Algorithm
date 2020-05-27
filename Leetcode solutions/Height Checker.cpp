fd87a30f928b248699c78951b7a3177fb4a23cdd
Vishal Yadav <52189323+Vishalyadav0804@users.noreply.github.com>
2020-05-27 08:01:06 +0530

    Create Height Checker.cpp

---
+++ Leetcode solutions/Height Checker.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int count=0;
+        vector<int> v;
+        for(int i=0;i<heights.size();i++){
+            v.push_back(heights[i]);
+        }
+        sort(v.begin(),v.end());
+        for(int i=0;i<heights.size();i++){
+            if(v[i]!=heights[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+};
