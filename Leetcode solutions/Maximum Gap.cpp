ac294abc0ea6272b7235530ed08ba6a3c09eacd0
Vishal Yadav <52189323+Vishalyadav0804@users.noreply.github.com>
2020-05-31 09:33:57 +0530

    Create Maximum Gap.cpp

---
+++ Leetcode solutions/Maximum Gap.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& A) {
+        if(A.size()<2) return 0;
+        vector<int> B = A;
+        sort(B.begin(),B.end());
+        int max = 0;
+        for(int i=0;i<B.size()-1;i++){
+            if(B[i+1]-B[i] > max)
+            max = B[i+1]-B[i];
+        }
+        return max;
+    }
+};
