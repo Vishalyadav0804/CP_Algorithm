5d12642c1fd22defab4d7582b172e52c86220997
Vishal Yadav <52189323+Vishalyadav0804@users.noreply.github.com>
2020-06-10 08:24:45 +0530

    Create Power of Four.cpp

---
+++ Leetcode solutions/Power of Four.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        while(num) {
+        if(num==1) return true;
+        if(num%4) return false;
+        num /= 4;
+    }
+    return false;
+    }
+};
