b5929a631fb0760724f888b5515252a590d46d0b
Vishal Yadav <52189323+Vishalyadav0804@users.noreply.github.com>
2020-06-23 09:14:54 +0530

    Create Search a 2-D matrix.cpp

---
+++ Leetcode solutions/Search a 2-D matrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int i = m-1,j = 0;
+        if(m == 0 ) return false;
+        int n = matrix[0].size();
+        
+        while( i >= 0 && j < n){
+            if( matrix[i][j] == target ) return true;
+            if( target > matrix[i][j]) j+=1;
+            else i-=1;
+        }
+        return false;
+    }
+};
