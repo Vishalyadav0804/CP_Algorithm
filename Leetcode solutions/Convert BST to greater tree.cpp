b654fa77a9953938b8fa7e118b58802f9dde686b
Vishal Yadav <52189323+Vishalyadav0804@users.noreply.github.com>
2020-07-06 19:11:53 +0530

    Create Convert BST to greater tree.cpp

---
+++ Leetcode solutions/Convert BST to greater tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void greaterTree(TreeNode* root,int &val){
+        if(!root) return;
+        greaterTree(root->right,val);
+        root->val=root->val+val;
+        val=root->val;
+        greaterTree(root->left,val);
+        return;
+    }
+    TreeNode* convertBST(TreeNode* root) {
+        int val=0;
+        greaterTree(root,val);
+        return root;
+    }
+};
