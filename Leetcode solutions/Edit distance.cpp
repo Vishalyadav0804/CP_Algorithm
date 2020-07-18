93f7ad7d01f5417d1e03156654615aaba1cb4a30
Vishal Yadav <52189323+Vishalyadav0804@users.noreply.github.com>
2020-07-18 07:53:47 +0530

    Create Edit distance.cpp

---
+++ Leetcode solutions/Edit distance.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int findDist(string word1,string word2,int m,int n){
+        int dp[m+1][n+1];
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0)
+                    dp[i][j]=j;
+                else if(j==0)
+                    dp[i][j]=i;
+                else if(word1[i-1]==word2[j-1])
+                    dp[i][j]=dp[i-1][j-1];
+                else
+                    dp[i][j]=1+min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1]));
+            }
+        }
+        return dp[m][n];
+    }
+    int minDistance(string word1, string word2) {
+        return findDist(word1,word2,word1.length(),word2.length());
+    }
+};
+
+/*
+int editDist(string str1, string str2, int m, int n) 
+{ 
+    // If first string is empty, the only option is to 
+    // insert all characters of second string into first 
+    if (m == 0) 
+        return n; 
+  
+    // If second string is empty, the only option is to 
+    // remove all characters of first string 
+    if (n == 0) 
+        return m; 
+  
+    // If last characters of two strings are same, nothing 
+    // much to do. Ignore last characters and get count for 
+    // remaining strings. 
+    if (str1[m - 1] == str2[n - 1]) 
+        return editDist(str1, str2, m - 1, n - 1); 
+  
+    // If last characters are not same, consider all three 
+    // operations on last character of first string, recursively 
+    // compute minimum cost for all three operations and take 
+    // minimum of three values. 
+    return 1 + min(editDist(str1, str2, m, n - 1), // Insert 
+                   editDist(str1, str2, m - 1, n), // Remove 
+                   editDist(str1, str2, m - 1, n - 1) // Replace 
+                   ); 
+} 
+*/
