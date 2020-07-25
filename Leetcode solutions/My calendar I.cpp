bdfdbf26b0a62c86dc7c5387973343f35c1d7572
Vishal Yadav <52189323+Vishalyadav0804@users.noreply.github.com>
2020-07-25 07:30:14 +0530

    Create My calendar I.cpp

---
+++ Leetcode solutions/My calendar I.cpp
@@ -0,0 +1,32 @@
+class MyCalendar {
+public:
+    MyCalendar() {
+        
+    }
+    bool canAdd(){
+       int count=0;
+       for (auto entry: m){
+           count+=entry.second;
+           if(count>1) return false;
+       }
+       return true;
+   }
+    map<int,int> m;
+    bool book(int start, int end) {
+        m[start]++;
+        m[end]--;
+        if (canAdd()) return true;
+        else{
+            //restore the original values since we are not booking this room
+            m[start]--;
+            m[end]++;
+            return false;
+        }
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
