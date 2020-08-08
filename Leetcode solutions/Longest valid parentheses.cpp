6608abef66ab4c66b17d2c0d33c4874740c59ab8
Vishal Yadav <52189323+Vishalyadav0804@users.noreply.github.com>
2020-08-08 08:11:06 +0530

    Create Longest valid parentheses.cpp

---
+++ Leetcode solutions/Longest valid parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> p;
+        int maxlength=0;
+        p.push(-1);
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                p.push(i);
+            }else{
+                p.pop();
+                if(p.empty()){
+                    p.push(i);
+                }else{
+                    maxlength=max(maxlength,i-p.top());
+                }
+            }
+        }
+        return maxlength;
+    }
+};
