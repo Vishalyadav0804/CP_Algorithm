a3bce4d47face0b3b31f9ec0f0fa2134a5819e3d
Vishal Yadav <52189323+Vishalyadav0804@users.noreply.github.com>
2020-08-21 06:57:47 +0530

    Create Valid mountain array.cpp

---
+++ Leetcode solutions/Valid mountain array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& A) {
+        if(A.size()<3) return false;
+        int pointer=1;
+        while(pointer<A.size()){
+            if(A[pointer-1]<A[pointer]) pointer++;
+            else break;
+        }
+        if(pointer==A.size() || pointer==1) return false;
+        while(pointer<A.size()){
+            if(A[pointer-1]>A[pointer]) pointer++;
+            else break;
+        }
+        if(pointer==A.size()) return true;
+        return false;
+    }
+};
