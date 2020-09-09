a5de8b83866a35d7a64e91cb8ac0c7f48dd68e4f
Vishal Yadav <52189323+Vishalyadav0804@users.noreply.github.com>
2020-09-09 06:55:25 +0530

    Create Sort array by parity.cpp

---
+++ Leetcode solutions/Sort array by parity.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+        int i=0,j=0;
+        while(j<A.size()){
+            if(A[j]%2==0){
+                swap(A[i],A[j]);
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        return A;
+    }
+};
